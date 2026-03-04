2c575084daafddb7b3a1eb637ea79dd59eaf8e14
Vrushabh Patil <patilvrushabh1008@gmail.com>
2026-03-04 18:45:13 +0530

    Create integration test for HomePage

--- ecommerce-project/src/pages/home/Product.jsx
+++ ecommerce-project/src/pages/home/Product.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { formatMoney } from "../../utils/money";
+import { test } from "./../../../node_modules/chai/lib/chai/utils/test";
 
 export function Product({ product, loadCart }) {
   const [quantity, setQuantity] = useState(1);
@@ -19,7 +20,7 @@ export function Product({ product, loadCart }) {
   };
 
   return (
-    <div className="product-container">
+    <div className="product-container" data-testid="product-container">
       <div className="product-image-container">
         <img
           className="product-image"
