5b936b7b6880497310c2edc8594978aa6d1c9b94
Vrushabh Patil <patilvrushabh1008@gmail.com>
2026-03-05 15:13:11 +0530

    HomePage and OrdersPage updated

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Header } from "../../components/Header.jsx";
+import { Header } from "../../components/Header";
 import "./HomePage.css";
 import { ProductsGrid } from "./ProductsGrid.jsx";
 
